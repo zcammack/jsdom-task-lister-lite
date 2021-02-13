4473ef07065a29184207eb48dbe9f85969fc7cfa
zcammack <zack.cammack@gmail.com>
2021-02-13 01:33:34 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById("create-task-form").addEventListener("submit", 
+  function(event) { 
+    event.preventDefault();
+}, false);
+
+  document.getElementById("create-task-form").addEventListener('submit', addTask); 
+  
+  function addTask() {
+    let task = document.getElementById("new-task-description").value;
+    let li = document.createElement("li");
+    li.innerText = task
+    document.getElementById("tasks").appendChild(li);
+  }
 });
